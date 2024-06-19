e8a9d8dcff2448ea7a4f8cf548abc70806a0ce9d
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-19 23:02:00 +0530

    stage 3 trail -1

--- app/main.py
+++ app/main.py
@@ -1,39 +1,28 @@
 import json
 import sys
 import bencodepy
-# import bencodepy - available if you need it!
-# import requests - available if you need it!
-# Examples:
-#
-# - decode_bencode(b"5:hello") -> b"hello"
-# - decode_bencode(b"10:hello12345") -> b"hello12345"
+
 def decode_bencode(bencoded_value):
     return bencodepy.decode(bencoded_value)
-    if chr(bencoded_value[0]).isdigit():
-        length = int(bencoded_value.split(b":")[0])
-        return bencoded_value.split(b":")[1][:length]
-    elif bencoded_value.startswith(b"i"):  # i.e. b'i52e'
-        return int(bencoded_value[1:-1])
+
+def bytes_to_str(data):
+    if isinstance(data, bytes):
+        return data.decode()
+    elif isinstance(data, list):
+        return [bytes_to_str(item) for item in data]
+    elif isinstance(data, dict):
+        return {bytes_to_str(key): bytes_to_str(value) for key, value in data.items()}
     else:
-        raise NotImplementedError("Only strings are supported at the moment")
+        raise TypeError(f"Type not serializable: {type(data)}")
+
 def main():
     command = sys.argv[1]
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
-        # json.dumps() can't handle bytes, but bencoded "strings" need to be
-        # bytestrings since they might contain non utf-8 characters.
-        #
-        # Let's convert them to strings for printing to the console.
-        def bytes_to_str(data):
-            if isinstance(data, bytes):
-                return data.decode()
-            raise TypeError(f"Type not serializable: {type(data)}")
-        # Uncomment this block to pass the first stage
-        print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
+        decoded_value = decode_bencode(bencoded_value)
+        print(json.dumps(decoded_value, default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
+
 if __name__ == "__main__":
     main()
-
