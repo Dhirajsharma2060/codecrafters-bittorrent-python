f9b07910a3ae8198ee66e131060d7fc5381ff51b
Dhiraj2060 <dhirajsharma2060@gmail.com>
2024-06-19 23:35:01 +0530

    stage 4 trail -1

--- app/main.py
+++ app/main.py
@@ -2,25 +2,29 @@ import json
 import sys
 import bencodepy
 
+# Initialize the bencodepy Bencode class
+bc = bencodepy.Bencode(encoding="utf-8")
+
 def decode_bencode(bencoded_value):
     return bencodepy.decode(bencoded_value)
 
 def bytes_to_str(data):
     if isinstance(data, bytes):
-        return data.decode()
+        return data.decode('utf-8')
     elif isinstance(data, list):
         return [bytes_to_str(item) for item in data]
     elif isinstance(data, dict):
-         # Ensure both keys and values are converted to strings
         return {bytes_to_str(key): bytes_to_str(value) for key, value in data.items()}
     else:
-        return data  # Return the data as is if it's not bytes, list, or dict
+        return data
+
 def main():
     command = sys.argv[1]
     if command == "decode":
         bencoded_value = sys.argv[2].encode()
         decoded_value = decode_bencode(bencoded_value)
-        print(json.dumps(decoded_value, default=bytes_to_str))
+        converted_value = bytes_to_str(decoded_value)
+        print(json.dumps(converted_value))
     else:
         raise NotImplementedError(f"Unknown command {command}")
 
